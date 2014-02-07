zturtleman/spearmint

commit 8e0cdaf765819b1631398622e84fa27786319b2a

Replace generic1 with team, tokens, and portalCullDistance

code/game/g_active.c
@@ -606,14 +606,14 @@ void ClientEvents( gentity_t *ent, int oldEventSequence ) {
 
 #ifdef MISSIONPACK
 			if ( g_gametype.integer == GT_HARVESTER ) {
-				if ( ent->client->ps.generic1 > 0 ) {
+				if ( ent->client->ps.tokens > 0 ) {
 					if ( ent->client->sess.sessionTeam == TEAM_RED ) {
 						item = BG_FindItem( "Blue Cube" );
 					} else {
 						item = BG_FindItem( "Red Cube" );
 					}
 					if ( item ) {
-						for ( j = 0; j < ent->client->ps.generic1; j++ ) {
+						for ( j = 0; j < ent->client->ps.tokens; j++ ) {
 							drop = Drop_Item( ent, item, 0 );
 							if ( ent->client->sess.sessionTeam == TEAM_RED ) {
 								drop->spawnflags = TEAM_BLUE;
@@ -622,7 +622,7 @@ void ClientEvents( gentity_t *ent, int oldEventSequence ) {
 							}
 						}
 					}
-					ent->client->ps.generic1 = 0;
+					ent->client->ps.tokens = 0;
 				}
 			}
 #endif

code/game/g_combat.c
@@ -151,7 +151,7 @@ void TossClientCubes( gentity_t *self ) {
 	vec3_t		angles;
 	vec3_t		origin;
 
-	self->client->ps.generic1 = 0;
+	self->client->ps.tokens = 0;
 
 	// this should never happen but we should never
 	// get the server to crash due to skull being spawned in
@@ -417,7 +417,7 @@ void CheckAlmostScored( gentity_t *self, gentity_t *attacker ) {
 	char		*classname;
 
 	// if the player was carrying cubes
-	if ( self->client->ps.generic1 ) {
+	if ( self->client->ps.tokens ) {
 		if ( self->client->sess.sessionTeam == TEAM_BLUE ) {
 			classname = "team_redobelisk";
 		}

code/cgame/cg_ents.c
@@ -495,7 +495,7 @@ static void CG_Missile( centity_t *cent ) {
 
 #ifdef MISSIONPACK
 	if ( cent->currentState.weapon == WP_PROX_LAUNCHER ) {
-		if (s1->generic1 == TEAM_BLUE) {
+		if (s1->team == TEAM_BLUE) {
 			ent.hModel = cgs.media.blueProxMine;
 		}
 	}

code/cgame/cg_newdraw.c
@@ -794,7 +794,7 @@ static void CG_HarvesterSkulls(rectDef_t *rect, float scale, vec4_t color, qbool
 	char num[16];
 	vec3_t origin, angles;
 	qhandle_t handle;
-	int value = cg.cur_ps->generic1;
+	int value = cg.cur_ps->tokens;
 
 	if (cgs.gametype != GT_HARVESTER) {
 		return;

code/cgame/cg_players.c
@@ -1774,7 +1774,7 @@ static void CG_PlayerTokens( centity_t *cent, int renderfx ) {
 	vec3_t		dir, origin;
 	skulltrail_t *trail;
 	trail = &cg.skulltrails[cent->currentState.number];
-	tokens = cent->currentState.generic1;
+	tokens = cent->currentState.tokens;
 	if ( !tokens ) {
 		trail->numpositions = 0;
 		return;

code/game/ai_dmnet.c
@@ -1399,7 +1399,7 @@ void BotClearPath(bot_state_t *bs, bot_moveresult_t *moveresult) {
 		}
 		if (bestmine != -1) {
 			//
-			// state->generic1 == TEAM_RED || state->generic1 == TEAM_BLUE
+			// state->team == TEAM_RED || state->team == TEAM_BLUE
 			//
 			// deactivate prox mines in the bot's path by shooting
 			// rockets or plasma cells etc. at them

code/game/ai_dmq3.c
@@ -315,7 +315,7 @@ qboolean EntityCarriesCubes(aas_entityinfo_t *entinfo) {
 		return qfalse;
 	//FIXME: get this info from the aas_entityinfo_t ?
 	BotAI_GetEntityState(entinfo->number, &state);
-	if (state.generic1 > 0)
+	if (state.tokens > 0)
 		return qtrue;
 	return qfalse;
 }
@@ -1787,12 +1787,12 @@ void BotUpdateInventory(bot_state_t *bs) {
 #ifdef MISSIONPACK
 	bs->inventory[INVENTORY_NEUTRALFLAG] = bs->cur_ps.powerups[PW_NEUTRALFLAG] != 0;
 	if (BotTeam(bs) == TEAM_RED) {
-		bs->inventory[INVENTORY_REDCUBE] = bs->cur_ps.generic1;
+		bs->inventory[INVENTORY_REDCUBE] = bs->cur_ps.tokens;
 		bs->inventory[INVENTORY_BLUECUBE] = 0;
 	}
 	else {
 		bs->inventory[INVENTORY_REDCUBE] = 0;
-		bs->inventory[INVENTORY_BLUECUBE] = bs->cur_ps.generic1;
+		bs->inventory[INVENTORY_BLUECUBE] = bs->cur_ps.tokens;
 	}
 #endif
 	BotCheckItemPickup(bs, oldinventory);
@@ -4737,7 +4737,7 @@ void BotCheckForProxMines(bot_state_t *bs, entityState_t *state) {
 	if (state->eType != ET_MISSILE || state->weapon != WP_PROX_LAUNCHER)
 		return;
 	// if this prox mine is from someone on our own team
-	if (state->generic1 == BotTeam(bs))
+	if (state->team == BotTeam(bs))
 		return;
 	// if the bot doesn't have a weapon to deactivate the mine
 	if (!(bs->inventory[INVENTORY_PLASMAGUN] > 0 && bs->inventory[INVENTORY_CELLS] > 0) &&

code/game/bg_misc.c
@@ -976,7 +976,8 @@ vmNetField_t	bg_entityStateFields[] =
 { NETF(modelindex), 8 },
 { NETF(otherEntityNum2), GENTITYNUM_BITS },
 { NETF(loopSound), 8 },
-{ NETF(generic1), 8 },
+{ NETF(tokens), 8 },
+{ NETF(team), 8 },
 { NETF(origin2[2]), 0 },
 { NETF(origin2[0]), 0 },
 { NETF(origin2[1]), 0 },
@@ -1045,7 +1046,7 @@ vmNetField_t	bg_playerStateFields[] =
 { PSF(damageYaw), 8 },
 { PSF(damagePitch), 8 },
 { PSF(damageCount), 8 },
-{ PSF(generic1), 8 },
+{ PSF(tokens), 8 },
 { PSF(pm_type), 8 },					
 { PSF(delta_angles[0]), 16 },
 { PSF(delta_angles[2]), 16 },
@@ -1261,10 +1262,7 @@ qboolean BG_CanItemBeGrabbed( int gametype, const entityState_t *ent, const play
 		}
 
 		// check team only
-		if( ( ent->generic1 & 2 ) && ( ps->persistant[PERS_TEAM] != TEAM_RED ) ) {
-			return qfalse;
-		}
-		if( ( ent->generic1 & 4 ) && ( ps->persistant[PERS_TEAM] != TEAM_BLUE ) ) {
+		if( ent->team != 255 && ( ps->persistant[PERS_TEAM] != ent->team ) ) {
 			return qfalse;
 		}
 
@@ -1674,7 +1672,8 @@ void BG_PlayerStateToEntityState( playerState_t *ps, entityState_t *s, qboolean 
 
 	s->contents = ps->contents;
 	s->loopSound = ps->loopSound;
-	s->generic1 = ps->generic1;
+	s->tokens = ps->tokens;
+	s->team = ps->persistant[PERS_TEAM];
 
 	s->bmodel = qfalse;
 	s->capsule = ps->capsule;
@@ -1765,7 +1764,8 @@ void BG_PlayerStateToEntityStateExtraPolate( playerState_t *ps, entityState_t *s
 
 	s->contents = ps->contents;
 	s->loopSound = ps->loopSound;
-	s->generic1 = ps->generic1;
+	s->tokens = ps->tokens;
+	s->team = ps->persistant[PERS_TEAM];
 
 	s->bmodel = qfalse;
 	s->capsule = ps->capsule;

zturtleman/mint-arena

commit 19243ec6d32b45bdb07a53cb995f3400209844ac

mint-arena: Fix harvester skull pickup
Both teams could pick up any skull and count toward held skulls.
Picking up your own color skull is suppose to just remove the entitiy.

Caused by commit 8e0cdaf765819b1631398622e84fa27786319b2a.

code/game/g_active.c
@@ -616,9 +616,9 @@ void ClientEvents( gentity_t *ent, int oldEventSequence ) {
 						for ( j = 0; j < ent->client->ps.tokens; j++ ) {
 							drop = Drop_Item( ent, item, 0 );
 							if ( ent->client->sess.sessionTeam == TEAM_RED ) {
-								drop->spawnflags = TEAM_BLUE;
+								drop->s.team = TEAM_BLUE;
 							} else {
-								drop->spawnflags = TEAM_RED;
+								drop->s.team = TEAM_RED;
 							}
 						}
 					}

code/game/g_combat.c
@@ -186,7 +186,7 @@ void TossClientCubes( gentity_t *self ) {
 
 	drop->nextthink = level.time + g_cubeTimeout.integer * 1000;
 	drop->think = G_FreeEntity;
-	drop->spawnflags = self->client->sess.sessionTeam;
+	drop->s.team = self->client->sess.sessionTeam;
 }
 
 
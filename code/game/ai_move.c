7aa622958a41fcbc821977cbd97e56acfb69cf59
Zack Middleton <zturtleman@gmail.com>
2014-04-14 16:04:22 -0500

    mint-arena: Remove unused function Intersection in ai_move.c

--- code/game/ai_move.c
+++ code/game/ai_move.c
@@ -1274,35 +1274,6 @@ int BotMoveInDirection(int movestate, vec3_t dir, float speed, int type)
 // Returns:				-
 // Changes Globals:		-
 //===========================================================================
-int Intersection(vec2_t p1, vec2_t p2, vec2_t p3, vec2_t p4, vec2_t out)
-{
-   float x1, dx1, dy1, x2, dx2, dy2, d;
-
-   dx1 = p2[0] - p1[0];
-   dy1 = p2[1] - p1[1];
-   dx2 = p4[0] - p3[0];
-   dy2 = p4[1] - p3[1];
-
-   d = dy1 * dx2 - dx1 * dy2;
-   if (d != 0)
-   {
-      x1 = p1[1] * dx1 - p1[0] * dy1;
-      x2 = p3[1] * dx2 - p3[0] * dy2;
-      out[0] = (int) ((dx1 * x2 - dx2 * x1) / d);
-      out[1] = (int) ((dy1 * x2 - dy2 * x1) / d);
-		return qtrue;
-   } //end if
-   else
-   {
-      return qfalse;
-   } //end else
-} //end of the function Intersection
-//===========================================================================
-//
-// Parameter:			-
-// Returns:				-
-// Changes Globals:		-
-//===========================================================================
 void BotCheckBlocked(bot_movestate_t *ms, vec3_t dir, int checkbottom, bot_moveresult_t *result)
 {
 	vec3_t mins, maxs, end, up = {0, 0, 1};

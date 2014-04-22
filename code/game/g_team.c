c8c63797998905bd22e33d3dfbeeac942f5acc9c
Zack Middleton <zturtleman@gmail.com>
2014-04-22 02:53:24 -0500

    mint-arena: Fix obelisk model not dropping to floor
    Team Arena's Overload gametype has red and blue team Obelisk base objects.
    It uses separate entities for visual and damage. Only the damageable entity was dropped to floor.
    Leaving model floating off the ground.

--- code/game/g_team.c
+++ code/game/g_team.c
@@ -1322,9 +1322,7 @@ static void ObeliskPain( gentity_t *self, gentity_t *attacker, int damage ) {
 	AddScore(attacker, self->r.currentOrigin, actualDamage);
 }
 
-gentity_t *SpawnObelisk( vec3_t origin, int team, int spawnflags) {
-	trace_t		tr;
-	vec3_t		dest;
+gentity_t *SpawnObelisk( vec3_t origin, vec3_t mins, vec3_t maxs, int team ) {
 	gentity_t	*ent;
 
 	ent = G_Spawn();
@@ -1333,8 +1331,8 @@ gentity_t *SpawnObelisk( vec3_t origin, int team, int spawnflags) {
 	VectorCopy( origin, ent->s.pos.trBase );
 	VectorCopy( origin, ent->r.currentOrigin );
 
-	VectorSet( ent->s.mins, -15, -15, 0 );
-	VectorSet( ent->s.maxs, 15, 15, 87 );
+	VectorCopy( mins, ent->s.mins );
+	VectorCopy( maxs, ent->s.maxs );
 
 	ent->s.eType = ET_GENERAL;
 	ent->flags = FL_NO_KNOCKBACK;
@@ -1353,7 +1351,25 @@ gentity_t *SpawnObelisk( vec3_t origin, int team, int spawnflags) {
 		ent->touch = ObeliskTouch;
 	}
 
-	if ( spawnflags & 1 ) {
+	G_SetOrigin( ent, ent->s.origin );
+
+	ent->spawnflags = team;
+
+	trap_LinkEntity( ent );
+
+	return ent;
+}
+
+void ObeliskInit( gentity_t *ent ) {
+	trace_t		tr;
+	vec3_t		dest;
+
+	ent->s.eType = ET_TEAM;
+
+	VectorSet( ent->s.mins, -15, -15, 0 );
+	VectorSet( ent->s.maxs, 15, 15, 87 );
+
+	if ( ent->spawnflags & 1 ) {
 		// suspended
 		G_SetOrigin( ent, ent->s.origin );
 	} else {
@@ -1377,12 +1393,6 @@ gentity_t *SpawnObelisk( vec3_t origin, int team, int spawnflags) {
 			G_SetOrigin( ent, tr.endpos );
 		}
 	}
-
-	ent->spawnflags = team;
-
-	trap_LinkEntity( ent );
-
-	return ent;
 }
 
 /*QUAKED team_redobelisk (1 0 0) (-16 -16 0) (16 16 8)
@@ -1394,16 +1404,16 @@ void SP_team_redobelisk( gentity_t *ent ) {
 		G_FreeEntity(ent);
 		return;
 	}
-	ent->s.eType = ET_TEAM;
+	ObeliskInit( ent );
 	if ( g_gametype.integer == GT_OBELISK ) {
-		obelisk = SpawnObelisk( ent->s.origin, TEAM_RED, ent->spawnflags );
+		obelisk = SpawnObelisk( ent->s.origin, ent->s.mins, ent->s.maxs, TEAM_RED );
 		obelisk->activator = ent;
 		// initial obelisk health value
 		ent->s.modelindex2 = 0xff;
 		ent->s.frame = 0;
 	}
 	if ( g_gametype.integer == GT_HARVESTER ) {
-		obelisk = SpawnObelisk( ent->s.origin, TEAM_RED, ent->spawnflags );
+		obelisk = SpawnObelisk( ent->s.origin, ent->s.mins, ent->s.maxs, TEAM_RED );
 		obelisk->activator = ent;
 	}
 	ent->s.modelindex = TEAM_RED;
@@ -1419,16 +1429,16 @@ void SP_team_blueobelisk( gentity_t *ent ) {
 		G_FreeEntity(ent);
 		return;
 	}
-	ent->s.eType = ET_TEAM;
+	ObeliskInit( ent );
 	if ( g_gametype.integer == GT_OBELISK ) {
-		obelisk = SpawnObelisk( ent->s.origin, TEAM_BLUE, ent->spawnflags );
+		obelisk = SpawnObelisk( ent->s.origin, ent->s.mins, ent->s.maxs, TEAM_BLUE );
 		obelisk->activator = ent;
 		// initial obelisk health value
 		ent->s.modelindex2 = 0xff;
 		ent->s.frame = 0;
 	}
 	if ( g_gametype.integer == GT_HARVESTER ) {
-		obelisk = SpawnObelisk( ent->s.origin, TEAM_BLUE, ent->spawnflags );
+		obelisk = SpawnObelisk( ent->s.origin, ent->s.mins, ent->s.maxs, TEAM_BLUE );
 		obelisk->activator = ent;
 	}
 	ent->s.modelindex = TEAM_BLUE;
@@ -1442,9 +1452,9 @@ void SP_team_neutralobelisk( gentity_t *ent ) {
 		G_FreeEntity(ent);
 		return;
 	}
-	ent->s.eType = ET_TEAM;
+	ObeliskInit( ent );
 	if ( g_gametype.integer == GT_HARVESTER) {
-		neutralObelisk = SpawnObelisk( ent->s.origin, TEAM_FREE, ent->spawnflags);
+		neutralObelisk = SpawnObelisk( ent->s.origin, ent->s.mins, ent->s.maxs, TEAM_FREE );
 		neutralObelisk->spawnflags = TEAM_FREE;
 	}
 	ent->s.modelindex = TEAM_FREE;

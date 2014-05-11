9c1d87c2f88df6ae5f64427a1c6ddc5c2dec6de0
Zack Middleton <zturtleman@gmail.com>
2014-05-11 17:32:49 -0500

    mint-arena: Don't build score info for bots, they don't parse it

--- code/game/g_cmds.c
+++ code/game/g_cmds.c
@@ -48,6 +48,11 @@ void DeathmatchScoreboardMessage( gentity_t *ent ) {
 	gclient_t	*cl;
 	int			numSorted, scoreFlags, accuracy, perfect;
 
+	// don't send scores to bots (bots don't parse them)
+	if ( ent->r.svFlags & SVF_BOT ) {
+		return;
+	}
+
 	// send the latest information on all clients
 	string[0] = 0;
 	stringlength = 0;

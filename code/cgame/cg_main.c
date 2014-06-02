1124c4e98f58659db1647164b9748f31320655a9
Zack Middleton <zturtleman@gmail.com>
2014-06-02 18:52:43 -0500

    mint-arena: Add support for engine to notify CGame that glconfig changed
    Called when r_fullscreen is changed if fast toggle is done instead of vid_restart.
    Called when window is resized (r_allowResize 1 and drag window edge).

--- code/cgame/cg_main.c
+++ code/cgame/cg_main.c
@@ -51,6 +51,7 @@ void CG_Refresh( int serverTime, stereoFrame_t stereoView, qboolean demoPlayback
 static char *CG_VoIPString( int localClientNum );
 static int CG_MousePosition( int localClientNum );
 static void CG_SetMousePosition( int localClientNum, int x, int y );
+static void CG_UpdateGlconfig( void );
 
 
 /*
@@ -118,6 +119,9 @@ Q_EXPORT intptr_t vmMain( int command, int arg0, int arg1, int arg2, int arg3, i
 		return 0;
 	case CG_CREATE_USER_CMD:
 		return (intptr_t)CG_CreateUserCmd(arg0, arg1, arg2, IntAsFloat(arg3), IntAsFloat(arg4), arg5);
+	case CG_UPDATE_GLCONFIG:
+		CG_UpdateGlconfig();
+		return 0;
 	default:
 		CG_Error( "cgame vmMain: unknown command %i", command );
 		break;
@@ -2477,12 +2481,7 @@ void CG_Init( connstate_t state, int maxSplitView, int playVideo ) {
 	cgs.media.nodrawShader		= trap_R_RegisterShaderEx( "nodraw", LIGHTMAP_NONE, qtrue );
 
 	// get the rendering configuration from the client system
-	trap_GetGlconfig( &cgs.glconfig );
-
-	// Viewport scale and offset
-	cg.viewport = 0;
-	cg.numViewports = 1;
-	CG_CalcVrect();
+	CG_UpdateGlconfig();
 
 	CG_ConsoleInit();
 
@@ -3132,3 +3131,20 @@ static char *CG_VoIPString( int localClientNum ) {
 
 	return voipString;
 }
+
+/*
+================
+CG_UpdateGlconfig
+================
+*/
+static void CG_UpdateGlconfig( void ) {
+	trap_GetGlconfig( &cgs.glconfig );
+
+	if ( cg.connState != CA_ACTIVE ) {
+		// Viewport scale and offset
+		cg.viewport = 0;
+		cg.numViewports = 1;
+		CG_CalcVrect();
+	}
+}
+

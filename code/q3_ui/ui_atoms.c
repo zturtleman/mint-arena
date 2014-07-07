7bc77acb16e8d93a84754e6dd4e4117873090a9c
Zack Middleton <zturtleman@gmail.com>
2014-07-07 18:56:19 -0500

    mint-arena: Remove unused incomplete global ranking system code
    The ranking system calls, commands, and cvars don't exist in latest Q3 release.
    Appearently it was removed when Team Arena was released (Q3 1.27g).

--- code/q3_ui/ui_atoms.c
+++ code/q3_ui/ui_atoms.c
@@ -792,11 +792,6 @@ void UI_SetActiveMenu( uiMenuCommand_t menu ) {
 		UI_MainMenu();
 		return;
 	case UIMENU_INGAME:
-		/*
-		//GRank
-		UI_RankingsMenu();
-		return;
-		*/
 		trap_Cvar_SetValue( "cl_paused", 1 );
 		UI_InGameMenu();
 		return;
--- code/q3_ui/ui_local.h
+++ code/q3_ui/ui_local.h
@@ -425,12 +425,6 @@ extern void UI_PreferencesMenu( void );
 extern void Preferences_Cache( void );
 
 //
-// ui_specifyleague.c
-//
-extern void UI_SpecifyLeagueMenu( void );
-extern void SpecifyLeague_Cache( void );
-
-//
 // ui_specifyserver.c
 //
 extern void UI_SpecifyServerMenu( void );
@@ -683,34 +677,5 @@ void UI_SPUnlock_f( void );
 void UI_SPUnlockMedals_f( void );
 
 void UI_InitGameinfo( void );
-
-//GRank
-
-//
-// ui_rankings.c
-//
-void Rankings_DrawText( void* self );
-void Rankings_DrawName( void* self );
-void Rankings_DrawPassword( void* self );
-void Rankings_Cache( void );
-void UI_RankingsMenu( void );
-
-//
-// ui_login.c
-//
-void Login_Cache( void );
-void UI_LoginMenu( void );
-
-//
-// ui_signup.c
-//
-void Signup_Cache( void );
-void UI_SignupMenu( void );
-
-//
-// ui_rankstatus.c
-//
-void RankStatus_Cache( void );
-void UI_RankStatusMenu( void );
 
 #endif

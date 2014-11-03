14e3c92ba781106851f55d76970b06ae4953c1f5
Zack Middleton <zturtleman@gmail.com>
2014-11-03 03:35:11 -0600

    mint-arena: Draw shadow on brush models
    Make trace for shadow ground collide with brush models.
    Requires previous commit for drawing marks on brush models.

--- code/cgame/cg_players.c
+++ code/cgame/cg_players.c
@@ -2214,7 +2214,7 @@ static qboolean CG_PlayerShadow( centity_t *cent, vec3_t start, float alphaMult,
 	VectorCopy( start, end );
 	end[2] -= SHADOW_DISTANCE;
 
-	trap_CM_BoxTrace( &trace, start, end, mins, maxs, 0, MASK_PLAYERSOLID );
+	CG_Trace( &trace, start, mins, maxs, end, cent->currentState.number, MASK_SOLID );
 
 	// no shadow if too high
 	if ( trace.fraction == 1.0 || trace.startsolid || trace.allsolid ) {

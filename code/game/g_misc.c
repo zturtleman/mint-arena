5b26946259cc5bb8e80cf59e8167234849dbefa5
Zack Middleton <zturtleman@gmail.com>
2014-12-08 04:38:21 -0600

    mint-arena: Fix unlagged TeleportPlayer
    Call G_ResetHistory after updating r.currentOrigin.

--- code/game/g_misc.c
+++ code/game/g_misc.c
@@ -117,10 +117,6 @@ void TeleportPlayer( gentity_t *player, vec3_t origin, vec3_t angles ) {
 
 	VectorCopy ( origin, player->player->ps.origin );
 	player->player->ps.origin[2] += 1;
-
-	// we don't want players being backward-reconciled back through teleporters
-	G_ResetHistory( player );
-
 	if (!noAngles) {
 	// spit the player out
 	AngleVectors( angles, player->player->ps.velocity, NULL, NULL );
@@ -146,6 +142,9 @@ void TeleportPlayer( gentity_t *player, vec3_t origin, vec3_t angles ) {
 	if ( player->player->sess.sessionTeam != TEAM_SPECTATOR ) {
 		trap_LinkEntity (player);
 	}
+
+	// we don't want players being backward-reconciled back through teleporters
+	G_ResetHistory( player );
 }
 
 

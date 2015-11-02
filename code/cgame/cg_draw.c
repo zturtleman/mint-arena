95963f09aad4d31c0198414e46a4081aa45e3960
Zack Middleton <zturtleman@gmail.com>
2015-11-02 03:57:21 -0600

    mint-arena: Draw disconnect icon over lagometer in Team Arena too

--- code/cgame/cg_draw.c
+++ code/cgame/cg_draw.c
@@ -1512,8 +1512,13 @@ static void CG_DrawDisconnect( void ) {
 
 	CG_SetScreenPlacement(PLACE_RIGHT, PLACE_BOTTOM);
 
+#ifdef MISSIONPACK_HUD
+	x = 640 - 48;
+	y = 480 - 144;
+#else
 	x = 640 - 48;
 	y = 480 - 48;
+#endif
 
 	CG_DrawPic( x, y, 48, 48, trap_R_RegisterShader("gfx/2d/net.tga" ) );
 }

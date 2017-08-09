e65f06138506bbff334dbbd7dae96bee0c44c991
Zack Middleton <zack@cloemail.com>
2017-08-09 18:34:26 -0500

    Remove newlines from chat messages in Game VM
    This has no affect due to ioquake3 engine already removing command
    separators (which includes newlines) in Cmd_Args_Sanitize().

--- code/game/g_cmds.c
+++ code/game/g_cmds.c
@@ -1007,6 +1007,16 @@ void G_Say( gentity_t *ent, gentity_t *target, int mode, const char *chatText ) 
 	}
 }
 
+static void SanitizeChatText( char *text ) {
+	int i;
+
+	for ( i = 0; text[i]; i++ ) {
+		if ( text[i] == '\n' || text[i] == '\r' ) {
+			text[i] = ' ';
+		}
+	}
+}
+
 
 /*
 ==================
@@ -1028,6 +1038,8 @@ static void Cmd_Say_f( gentity_t *ent, int mode, qboolean arg0 ) {
 	{
 		p = ConcatArgs( 1 );
 	}
+
+	SanitizeChatText( p );
 
 	G_Say( ent, NULL, mode, p );
 }
@@ -1060,6 +1072,8 @@ static void Cmd_Tell_f( gentity_t *ent ) {
 	}
 
 	p = ConcatArgs( 2 );
+
+	SanitizeChatText( p );
 
 	G_Say( ent, target, SAY_TELL, p );
 }
@@ -1173,6 +1187,8 @@ static void Cmd_Voice_f( gentity_t *ent, int mode, qboolean arg0, qboolean voice
 		p = ConcatArgs( 1 );
 	}
 
+	SanitizeChatText( p );
+
 	G_Voice( ent, NULL, mode, p, voiceonly );
 }
 
@@ -1204,6 +1220,8 @@ static void Cmd_VoiceTell_f( gentity_t *ent, qboolean voiceonly ) {
 	}
 
 	id = ConcatArgs( 2 );
+
+	SanitizeChatText( id );
 
 	G_LogPrintf( "vtell: %s to %s: %s\n", ent->player->pers.netname, target->player->pers.netname, id );
 	G_Voice( ent, target, SAY_TELL, id, voiceonly );

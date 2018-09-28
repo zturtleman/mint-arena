fe2c63778ca4487149314377d076563d9a9071e7
Zack Middleton <zack@cloemail.com>
2018-09-27 21:13:13 -0500

    mint-arena: Change version for 1.0-devil

--- code/game/bg_public.h
+++ code/game/bg_public.h
@@ -44,7 +44,7 @@ Suite 120, Rockville, Maryland 20850 USA.
 
 // Keep this in-sync with VERSION in Makefile.
 #ifndef PRODUCT_VERSION
-	#define PRODUCT_VERSION			"0.6"
+	#define PRODUCT_VERSION			"1.0-devil"
 #endif
 
 // because games can change separately from the main system protocol, we need a
